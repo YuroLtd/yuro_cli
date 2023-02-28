9b03b1e6cb5d268144fd5cee25014b4002547c31
dugang <timor.du@hotmail.com>
2023-02-28 11:45:29 +0800

    增加android的build参数

--- lib/command/impl/build/build_android.dart
+++ lib/command/impl/build/build_android.dart
@@ -22,6 +22,13 @@ class BuildAndroid extends Command {
     ..addOption('flavor', abbr: 'f', help: '''
     Build a custom app flavor as defined by platform-specific build setup.
     Supports the use of product flavors in Android Gradle scripts, and the use of custom Xcode schemes.
+    ''')
+    ..addOption('target-platform', abbr: 'p', help: '''
+    The target platform for which the app is compiled.
+    [android-arm (default), android-arm64 (default), android-x86, android-x64 (default)]
+    ''')
+    ..addFlag('split-per-abi', abbr: 's', help: '''
+    Whether to split the APKs per ABIs. To learn more, see: https://developer.android.com/studio/build/configure-apk-splits#configure-abi-s
     ''');
 
   @override
@@ -34,10 +41,17 @@ class BuildAndroid extends Command {
     // 开始构建编译命令
     final executableArguments = ['build', 'apk'];
     final flavor = argResults['flavor'];
-    if (flavor != null) executableArguments.add('--flavor $flavor');
+    if (flavor != null) executableArguments.add('--flavor=$flavor');
 
     final target = argResults['target=<path>'];
-    if (target != null) executableArguments.addAll(['-t', target]);
+    if (target != null) executableArguments.add('--target=$target');
+
+    final platform = argResults['target-platform'];
+    if (platform != null) executableArguments.add('--target-platform=$platform');
+
+    final split = argResults['split-per-abi'];
+    if (split) executableArguments.add('--split-per-abi');
+
     await runExecutableArguments('flutter', executableArguments, verbose: true);
   }
 }
