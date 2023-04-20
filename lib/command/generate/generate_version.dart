1c7b5a8341c54c1bedd2400a7536e35fc214ae0c
dugang <timor.du@hotmail.com>
2023-04-20 11:04:19 +0800

    修复`yuro gen version`不能稳定输出内容的bug

--- lib/command/generate/generate_version.dart
+++ lib/command/generate/generate_version.dart
@@ -16,13 +16,15 @@ class GenerateVersion extends Command {
     // 创建build_config.g.dart
     final generateFile = File(join(await PROJECT_PATH, 'lib/generated/build.g.dart'));
     if (!generateFile.existsSync()) generateFile.createSync(recursive: true);
-
     final contents = generateFile.readAsLinesSync();
+    contents.forEach((element) {
+      print('"$element"');
+    });
+    print('/////////');
     // 判断是否要插入license
-    if (!contents.contains(license)) contents.insert(0, '$license\n\n');
-
-    // // 更新数字版本号
-    // await _updateVersionCode();
+    if (!contents.contains(license)) {
+      contents.insertAll(0, [license, '']);
+    }
 
     _addOrReplace(contents, 'buildBranch', await gitBranch());
     _addOrReplace(contents, 'buildNumber', await gitShort());
@@ -38,18 +40,18 @@ class GenerateVersion extends Command {
     _addOrReplace(contents, 'versionName', versionName);
     _addOrReplace(contents, 'versionCode', versionCode);
 
-    await generateFile.writeAsString(contents.join(), flush: true);
+    await generateFile.writeAsString(contents.join('\n'), flush: true);
 
-    editor.update(['version'], YamlScalar.wrap('$versionName${version.contains('+') ? '+$versionCode' : ''}'));
-    await yamlFile.writeAsString(editor.toString());
-    await runPubGet();
+    // editor.update(['version'], YamlScalar.wrap('$versionName${version.contains('+') ? '+$versionCode' : ''}'));
+    // await yamlFile.writeAsString(editor.toString());
+    // await runPubGet();
 
     logger.i('\rProcess finished with exit code 0');
   }
 
   void _addOrReplace(List<String> contents, String field, String value) {
     final index = contents.indexWhere((element) => element.contains(field));
-    final content = 'const $field = \'$value\';\n';
+    final content = 'const $field = \'$value\';';
     // 不存在, 在文件末尾追加一行, 否则替换原来的行
     index == -1 ? contents.add(content) : contents.replaceRange(index, index + 1, [content]);
   }
